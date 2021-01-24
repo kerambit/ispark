4346f7ef2b8f79924a11603a15b594c486c114e4
kerambit228 <QazwsxedC899295>
2021-01-24 10:25:08 +0300

    remove unused code in api routes

--- routes/api.php
+++ routes/api.php
@@ -17,10 +17,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::middleware('auth:api')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
-
 Route::apiResource('authors', AuthorController::class);
 Route::apiResource('books', BookController::class);
 Route::apiResource('ratings', RatingController::class)->only(['store']);
